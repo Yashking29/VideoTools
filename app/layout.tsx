8fe0d389a6af24799901aa84b00a8e11e8d8e5dc
Yash Hissaria <yash.hissaria@hirequotient.com>
2026-06-07 18:26:37 +0530

    Add Google Analytics G-1W90F38J9L

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -53,6 +54,7 @@ export default function RootLayout({
         <main className="flex-1">{children}</main>
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-1W90F38J9L" />
     </html>
   );
 }
